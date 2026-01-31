732cde9fa48633f04bbdda036130a909195bdfde
Nanthitha Venkatachapathy <nanthithavenkatachapathy@Nanthithas-MacBook-Air.local>
2026-01-31 15:20:19 +0530

    Fix jwt import to use python-jose library

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import jwt
+from jose import jwt
 import datetime
 
 app = FastAPI()
